94cd7bc211328b70783aca31c08b56361ba9b857
Luke Wilkins <luke@vectormediagroup.com>
2012-02-15 09:04:52 -0500

    Checking if Matrix is in place before attempting to bind the display event.

--- themes/third_party/vmg_chosen_member/vmg_chosen_member.js
+++ themes/third_party/vmg_chosen_member/vmg_chosen_member.js
@@ -19,30 +19,32 @@ $(document).ready(function(){
 		$('#hold_field_' + $('#vmg_chosen_member_' + unique_id + '_field_id').val()).css('overflow', 'visible');
 	});
     
-    Matrix.bind("vmg_chosen_member", "display", function(cell){
-        if (cell.row.isNew) {
-            var current_field = $(cell.dom.$td).find('.vmg-chosen-member');
-            var unique_id = current_field.attr('rel') + '_new' + cell.row.index;
-            
-            // Get out quick if no rel data
-            if (unique_id == '') { return true; }
-            
-            current_field.attr('id', 'vmg_chosen_member_' + unique_id);
-            $('#vmg_chosen_member_' + current_field.attr('rel') + '_field_id').attr('id', 'vmg_chosen_member_' + unique_id + '_field_id');
-            $('#vmg_chosen_member_' + current_field.attr('rel') + '_max_selections').attr('id', 'vmg_chosen_member_' + unique_id + '_max_selections');
-            $('#vmg_chosen_member_' + current_field.attr('rel') + '_placeholder_text').attr('id', 'vmg_chosen_member_' + unique_id + '_placeholder_text');
+    if (typeof Matrix != 'undefined') {
+        Matrix.bind("vmg_chosen_member", "display", function(cell){
+            if (cell.row.isNew) {
+                var current_field = $(cell.dom.$td).find('.vmg-chosen-member');
+                var unique_id = current_field.attr('rel') + '_new' + cell.row.index;
+                
+                // Get out quick if no rel data
+                if (unique_id == '') { return true; }
+                
+                current_field.attr('id', 'vmg_chosen_member_' + unique_id);
+                $('#vmg_chosen_member_' + current_field.attr('rel') + '_field_id').attr('id', 'vmg_chosen_member_' + unique_id + '_field_id');
+                $('#vmg_chosen_member_' + current_field.attr('rel') + '_max_selections').attr('id', 'vmg_chosen_member_' + unique_id + '_max_selections');
+                $('#vmg_chosen_member_' + current_field.attr('rel') + '_placeholder_text').attr('id', 'vmg_chosen_member_' + unique_id + '_placeholder_text');
 
-            var vmgcm_max_selections = $('#vmg_chosen_member_' + unique_id + '_max_selections').val();
-            var vmgcm_placeholder_text = $('#vmg_chosen_member_' + unique_id + '_placeholder_text').val();
-            var vmgcm_json_url = $(this).find('.vmg_chosen_member_json_url').val() + '&field_id=' + $('#vmg_chosen_member_' + unique_id + '_field_id').val();
-            
-            $('#vmg_chosen_member_' + unique_id).chosen(vmgcm_json_url, {
-                max_selections: vmgcm_max_selections,
-                placeholder_text: vmgcm_placeholder_text,
-            });
+                var vmgcm_max_selections = $('#vmg_chosen_member_' + unique_id + '_max_selections').val();
+                var vmgcm_placeholder_text = $('#vmg_chosen_member_' + unique_id + '_placeholder_text').val();
+                var vmgcm_json_url = $(this).find('.vmg_chosen_member_json_url').val() + '&field_id=' + $('#vmg_chosen_member_' + unique_id + '_field_id').val();
+                
+                $('#vmg_chosen_member_' + unique_id).chosen(vmgcm_json_url, {
+                    max_selections: vmgcm_max_selections,
+                    placeholder_text: vmgcm_placeholder_text,
+                });
 
-            $('#hold_field_' + $('#vmg_chosen_member_' + unique_id + '_field_id').val()).css('overflow', 'visible');
-        }
-    });
+                $('#hold_field_' + $('#vmg_chosen_member_' + unique_id + '_field_id').val()).css('overflow', 'visible');
+            }
+        });
+    }
 
 });
